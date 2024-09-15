ba974d4f28a0e6507cfcb23dfba524ff40ae73d7
Morteza Raeisi <raeisimv@gmail.com>
2024-09-15 12:19:45 +0330

    make the crate a no_std module

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@
 //! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
 //!
 //! A better way to format and display time. This crate converts 33023448000 to 1y 17d 5h 10m 48s
+
+#![no_std]
+extern crate alloc;
+
 pub use formatter::MillisecondPart;
 pub use splitter::Millisecond;
 
