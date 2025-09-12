d38430ce76cfa3dccf303d64ca6389304a2107db
Morteza Raeisi <mrraeisi@outlook.com>
2025-09-12 23:16:09 +0330

    add unit test for separating milliseconds

--- src/pretty/mod.rs
+++ src/pretty/mod.rs
@@ -8,3 +8,20 @@ pub use formatter::*;
 pub use parser::*;
 pub use splitter::*;
 pub use utils::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_have_separate_millisecond() {
+        assert_eq!(Millisecond::from_millis(1100).pretty(), "1.1s");
+        assert_eq!(
+            Millisecond::from_millis(1100).pretty_with(MillisecondOption {
+                separate_milliseconds: true,
+                ..Default::default()
+            }),
+            "1s 100ms"
+        );
+    }
+}
