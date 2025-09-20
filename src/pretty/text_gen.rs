115cf2fbe16bb44bd340393b420bd4f2f95717d8
Morteza Raeisi <mrraeisi@outlook.com>
2025-09-20 21:03:48 +0330

    use copied value instead of references in creating lables

--- src/pretty/text_gen.rs
+++ src/pretty/text_gen.rs
@@ -3,61 +3,61 @@ use alloc::{format, string::String};
 use crate::{MillisecondOption, pretty::MillisecondPart};
 
 pub(crate) fn get_part_long_label(part: &MillisecondPart, opt: &MillisecondOption) -> String {
-    match part {
+    match *part {
         MillisecondPart::Years(x) => {
-            if *x != 1 {
+            if x != 1 {
                 format!("{x} years")
             } else {
                 format!("{x} year")
             }
         }
         MillisecondPart::Days(x) => {
-            if *x != 1 {
+            if x != 1 {
                 format!("{x} days")
             } else {
                 format!("{x} day")
             }
         }
         MillisecondPart::Hours(x) => {
-            if *x != 1 {
+            if x != 1 {
                 format!("{x} hours")
             } else {
                 format!("{x} hour")
             }
         }
         MillisecondPart::Minutes(x) => {
-            if *x != 1 {
+            if x != 1 {
                 format!("{x} minutes")
             } else {
                 format!("{x} minute")
             }
         }
         MillisecondPart::Seconds(x) => {
-            if *x != 1 {
+            if x != 1 {
                 format!("{x} seconds")
             } else {
                 format!("{x} second")
             }
         }
         MillisecondPart::SecondsAndMs(secs, millis) => {
-            format!("{} seconds", combine_secs_and_millis(*secs, *millis, opt))
+            format!("{} seconds", combine_secs_and_millis(secs, millis, opt))
         }
         MillisecondPart::Millis(x) => {
-            if *x != 1 {
+            if x != 1 {
                 format!("{x} milliseconds")
             } else {
                 format!("{x} millisecond")
             }
         }
         MillisecondPart::Micros(x) => {
-            if *x != 1 {
+            if x != 1 {
                 format!("{x} microseconds")
             } else {
                 format!("{x} microsecond")
             }
         }
         MillisecondPart::Nanos(x) => {
-            if *x != 1 {
+            if x != 1 {
                 format!("{x} nanoseconds")
             } else {
                 format!("{x} nanosecond")
@@ -66,14 +66,14 @@ pub(crate) fn get_part_long_label(part: &MillisecondPart, opt: &MillisecondOptio
     }
 }
 pub(crate) fn get_part_short_label(part: &MillisecondPart, opt: &MillisecondOption) -> String {
-    match part {
+    match *part {
         MillisecondPart::Years(x) => format!("{x}y"),
         MillisecondPart::Days(x) => format!("{x}d"),
         MillisecondPart::Hours(x) => format!("{x}h"),
         MillisecondPart::Minutes(x) => format!("{x}m"),
         MillisecondPart::Seconds(x) => format!("{x}s"),
         MillisecondPart::SecondsAndMs(secs, millis) => {
-            format!("{}s", combine_secs_and_millis(*secs, *millis, opt))
+            format!("{}s", combine_secs_and_millis(secs, millis, opt))
         }
         MillisecondPart::Millis(x) => format!("{x}ms"),
         MillisecondPart::Micros(x) => format!("{x}µs"),
