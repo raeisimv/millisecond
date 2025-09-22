854b01caf4155caf1d1f5c34344f03ea293a364a
Morteza Raeisi <mrraeisi@outlook.com>
2025-09-22 21:06:06 +0330

    add option to customize the separator

--- src/pretty/mod.rs
+++ src/pretty/mod.rs
@@ -180,4 +180,31 @@ mod tests {
             assert_eq!(act, expected);
         }
     }
+    #[ignore = "for test"]
+    #[test]
+    fn should_customize_separator() {
+        use crate::pretty::OutputFormat::*;
+        use crate::pretty::Separator::*;
+
+        let cases = [
+            (1000 * 60 * 67 * 24 * 465, Short, Default, "1y 154d 6h"),
+            (1000 * 60 * 67 * 24 * 465, Colon, Default, "1y:154d:6h"), // override
+            (1000 * 60 * 67 * 24 * 465, Short, Space, "1y 154d 6h"),
+            (1000 * 60 * 67 * 24 * 465, Colon, Space, "1y 154d 6h"), // do not override
+            (1000 * 60 * 67 * 24 * 465, Short, SingleColon, "1y:154d:6h"),
+            (1000 * 60 * 67 * 24 * 465, Short, Comma, "1y,154d,6h"),
+            (1000 * 60 * 67 * 24 * 465, Short, Dash, "1y-154d-6h"),
+            (1000 * 60 * 67 * 24 * 465, Short, Pipe, "1y|154d|6h"),
+            (1000 * 60 * 67 * 24 * 465, Short, Underscore, "1y_154d_6h"),
+        ];
+        for (millis, format, separator, expected) in cases {
+            let opt = MillisecondOption {
+                format,
+                separator,
+                ..MillisecondOption::default()
+            };
+            let act = Millisecond::from_millis(millis).pretty_with(opt);
+            assert_eq!(act, expected);
+        }
+    }
 }
